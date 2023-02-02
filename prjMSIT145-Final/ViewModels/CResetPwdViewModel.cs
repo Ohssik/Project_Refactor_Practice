3790f5ceb8c8e8f88ff54faba9144f5969d02b36
Student_MSIT145_07 <Student@DESKTOP-EKRVM5K>
2023-02-02 20:54:35 +0800

    忘記密碼

---
+++ prjMSIT145-Final/ViewModels/CResetPwdViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace prjMSIT145_Final.ViewModels
+{
+    public class CResetPwdViewModel
+    {
+        public string txtAccount { get; set; }
+        public string txtPassword { get; set; }
+        public string txtConfirmPwd { get; set; }
+        public string token { get; set; }
+    }
+}
