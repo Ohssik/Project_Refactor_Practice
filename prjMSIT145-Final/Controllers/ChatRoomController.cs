64c201b6c7913cfd566912bc71d9dfc2ecce95c2
浩恩 <ms60416@GMAIL.COM>
2023-02-06 21:41:28 +0800

    新增聊天訊息儲存

---
+++ prjMSIT145-Final/Controllers/ChatRoomController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjMSIT145_Final.Models;
+using System.Runtime.CompilerServices;
+
+namespace prjMSIT145_Final.Controllers
+{
+    public class ChatRoomController : Controller
+    {
+
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+       
+    }
+}
