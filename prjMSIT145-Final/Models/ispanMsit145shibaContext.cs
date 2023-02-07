25f1b81e402cbd7c71666e92f8666e056a9b641c
ringo0816 <ringodarma@gmail.com>
2023-02-07 10:29:47 +0800

    更新資料庫

--- prjMSIT145-Final/Models/ispanMsit145shibaContext.cs
+++ prjMSIT145-Final/Models/ispanMsit145shibaContext.cs
@@ -48,14 +48,6 @@ namespace prjMSIT145_Final.Models
         public virtual DbSet<ViewShowFullOrder> ViewShowFullOrders { get; set; } = null!;
         public virtual DbSet<ViewShowProductList> ViewShowProductLists { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=ispan-msit145-shiba2.database.windows.net;Initial Catalog=ispanMsit145shiba;Persist Security Info=True;User ID=msit145Shiba;Password=sh1baMsite45");
-            }
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
