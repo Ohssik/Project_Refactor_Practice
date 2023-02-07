e77c9e4d0110a326d3f52f2ef852734b4452bbf9
chiao85 <chiaoqaz@gmail.com>
2023-02-07 20:45:22 +0800

    姵瑋_機器人,取得使用者頭像

--- prjMSIT145-Final/wwwroot/js/bot.js
+++ prjMSIT145-Final/wwwroot/js/bot.js
@@ -1,50 +1,65 @@
-﻿
-    const msgDiv = document.querySelector(".msgDiv"); //對話框
-    const userImg = document.querySelector("#userImg"); //客戶頭像
-    const txt = document.querySelector("#txt"); //輸入框
-    const btnSubmit = document.querySelector("#btnSubmit"); //送出
-    const tr = document.querySelectorAll("tr");
-    const resultDiv = document.querySelector("#resultDiv");
+﻿const msgDiv = document.querySelector(".msgDiv"); //對話框
+const userImg = document.querySelector("#userImg"); //客戶頭像
+const txt = document.querySelector("#txt"); //輸入框
+const btnSubmit = document.querySelector("#btnSubmit"); //送出
+const tr = document.querySelectorAll("tr");
+const resultDiv = document.querySelector("#resultDiv");
 
-    let msg = "";
-    // 點選常見問題
-    tr.forEach(function (td) {
-        td.addEventListener("click", function () {
-            console.log(this.firstElementChild.innerHTML);
-            msg = this.firstElementChild.innerHTML;
-            txt.value = msg;
-        });
-        });
+let msg = "";
+// 點選常見問題
+tr.forEach(function (td) {
+    td.addEventListener("click", function () {
+        console.log(this.firstElementChild.innerHTML);
+        msg = this.firstElementChild.innerHTML;
+        txt.value = msg;
+    });
+});
 
-    //按下送出鈕
-    btnSubmit.addEventListener("click", async function () {
-            if (txt.value === "") {
+//按下送出鈕
+btnSubmit.addEventListener("click", async function () {
+
+    //使用者頭像
+    const response = await fetch("/Problem/PUserImg");
+    const img = await response.json();
+    //Customer/Member
+
+
+    console.log(img);
+
+    if (txt.value === "") {
         alert("請輸入內容")
 
-                return false;
-            }
+        return false;
+    }
     let fragForUser = document.createDocumentFragment();
     let fragForService = document.createDocumentFragment();
 
-    const request = await fetch(`@Url.Content("~/Problem/PAnswer?keyword=${txt.value}")`);
+    const request = await fetch(`/Problem/PAnswer?keyword=${txt.value}`);
     const data = await request.json();
-    console.log(data)
+    //console.log(data)
     fragForService = `<div class="display serviceDiv">
-        <div class="imgDiv"><img src="@Url.Content("~/images/Problem/dailyLogo.png")" alt="ServiceImg" /></div>
-        <div class="serviceMsg">
-            ${data.answer}
-        </div>
-    </div>
-</div>`;
+                     <div class="imgDiv"><img src="../images/Problem/dailyLogo.png" alt="ServiceImg" /></div>
+                    <div class="serviceMsg">
+                               ${data.answer}
+                    </div>
+                    </div>`;
 
-            fragForUser = `< div class="display userDiv" >
+    //判斷使用者有無頭像
+    if (img.userImg !== "") {
+        fragForUser = `<div class="display userDiv">
                                     <div class="userMsg">${txt.value}</div>
-                                        <div class="imgDiv"><img src="@Url.Content("~/images/Problem/user.png")" alt="UserImg" /></div>
+                                        <div class="imgDiv" id='userImg'><img src="../images/Customer/Member/${img.userImg}" alt="SessionImg"/></div>
                                     </div > `;
+    }
+    else {
+        fragForUser = `<div class="display userDiv">
+                                    <div class="userMsg">${txt.value}</div>
+                                        <div class="imgDiv" id='userImg'><img src="../images/Problem/user.png" alt="UserImg" /></div>
+                                    </div > `;
+    }
+    resultDiv.innerHTML += fragForUser;
+    resultDiv.innerHTML += fragForService;
+    msgDiv.scrollTop = msgDiv.scrollHeight;
 
-            resultDiv.innerHTML += fragForUser;
-            resultDiv.innerHTML += fragForService;
-            msgDiv.scrollTop = msgDiv.scrollHeight;
-
-            txt.value = "";
-    });
+    txt.value = "";
+});
