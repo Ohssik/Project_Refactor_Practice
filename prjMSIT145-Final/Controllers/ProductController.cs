0d6baca37f3b007ef54b9c1d0f497e8b0dcbadb9
chiao85 <chiaoqaz@gmail.com>
2023-02-08 23:47:43 +0800

    姵瑋_微調

--- prjMSIT145-Final/Controllers/ProductController.cs
+++ prjMSIT145-Final/Controllers/ProductController.cs
@@ -111,7 +111,7 @@ namespace prjMSIT145_Final.Controllers
 					}
 					vm.product.Photo = fileName;
 				}
-				var proCFid = _context.ProductCategories.FirstOrDefault(o => o.CategoryName == vm.CategoryName);
+				var proCFid = _context.ProductCategories.FirstOrDefault(o => o.CategoryName == vm.CategoryName && o.BFid == vm.BFid);
 				vm.product.CategoryFid = proCFid.Fid;
 				vm.product.BFid = vm.BFid;
 				_context.Products.Add(vm.product);
