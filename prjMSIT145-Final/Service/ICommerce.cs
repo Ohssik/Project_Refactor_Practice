62cd28af863153a9e66b0f4b81e129f76ec999c3
chiao85 <chiaoqaz@gmail.com>
2023-02-12 22:44:58 +0800

    姵瑋_綠界金流(先不into到master)

---
+++ prjMSIT145-Final/Service/ICommerce.cs
@@ -0,0 +1,12 @@
+﻿using prjMSIT145_Final.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace prjMSIT145_Final.Service
+{
+	public interface ICommerce
+	{
+		string GetCallBack(SendToNewebPayIn inModel);
+		//string GetPeriodCallBack(SendToNewebPayIn inModel);
+		Result GetCallbackResult(IFormCollection form);
+	}
+}
---
+++ prjMSIT145-Final/Service/NewebPayReturn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace prjMSIT145_Final.Service
+{
+	public class NewebPayReturn<T>
+	{
+		public string Status { get; set; }
+		public string Message { get; set; }
+		public T Result { get; set; }
+	}
+
+	public class NewebPayQueryResult
+	{
+		public string MerchantID { get; set; }
+		public string MerchantOrderNo { get; set; }
+		public string TradeNo { get; set; }
+		public int Amt { get; set; }
+		public string TradeStatus { get; set; }
+		public string PaymentType { get; set; }
+		public DateTime CreateTime { get; set; }
+		public DateTime PayTime { get; set; }
+		public string CheckCode { get; set; }
+		public DateTime FundTime { get; set; }
+		public string ShopMerchantID { get; set; }
+
+	}
+}
