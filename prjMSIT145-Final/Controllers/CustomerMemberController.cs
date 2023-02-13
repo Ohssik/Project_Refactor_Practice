9752e322dade02ac32ad4cd49b2d56f50a935016
ALANLIN0927 <a29803385@hotmail.com>
2023-02-13 20:12:24 +0800

    增加Line登入鈕

--- prjMSIT145-Final/Controllers/CustomerMemberController.cs
+++ prjMSIT145-Final/Controllers/CustomerMemberController.cs
@@ -3,16 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.Scripting;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using Newtonsoft.Json;
 using NuGet.Protocol;
 using prjMSIT145_Final.Models;
 using prjMSIT145_Final.ViewModels;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Diagnostics.Metrics;
 using System.Net;
 using System.Net.Mail;
 using System.Security.Principal;
+using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using System.Web;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace prjMSIT145_Final.Controllers
 {
@@ -47,7 +52,7 @@ namespace prjMSIT145_Final.Controllers
                 if (x.Password.Equals(vm.txtPassword) && x.Phone.Equals(vm.txtAccount))
                 {
                     if (x.IsSuspensed == 0) {
-                        string json = JsonSerializer.Serialize(x);
+                        string json = System.Text.Json.JsonSerializer.Serialize(x);
                         HttpContext.Session.SetString(CDictionary.SK_LOGINED_USER, json);
 
                         return Redirect("~/Home/CIndex");
@@ -95,12 +100,12 @@ namespace prjMSIT145_Final.Controllers
             if (payload == null)
             {
                 // 驗證失敗
-                ViewData["Msg"] = "驗證 Google 授權失敗";
+               
                 return Redirect("~/Home/CIndex");
             }
             else
             {
-                NormalMember member = _context.NormalMembers.FirstOrDefault(c => c.Email == payload.Email);
+                NormalMember member = _context.NormalMembers.FirstOrDefault(c => c.GoogleEmail == payload.Email);
                 if (member != null)
                 {
                     CLoginViewModel vm = new CLoginViewModel();
@@ -112,7 +117,7 @@ namespace prjMSIT145_Final.Controllers
                 {
                     NormalMember x = new NormalMember();
 
-                    x.Email = payload.Email;
+                    x.GoogleEmail = payload.Email;
                     x.MemberName = payload.Name;
 
                     return RedirectToAction("Register", x);
@@ -180,6 +185,99 @@ namespace prjMSIT145_Final.Controllers
             return payload;
         }
 
+        public ActionResult LineLoginDirect()                                                                                    //line
+        {
+            string response_type = "code";
+            string client_id = "1657900734";
+            string redirect_uri = HttpUtility.UrlEncode("https://localhost:7266/CustomerMember/CALLBACKLOGIN");
+            string state = "aaa";
+            string LineLoginUrl = string.Format("https://access.line.me/oauth2/v2.1/authorize?response_type={0}&client_id={1}&redirect_uri={2}&state={3}&scope=openid%20profile&nonce=09876xyz",
+                response_type,
+                client_id,
+                redirect_uri,
+                state
+                );
+            return Redirect(LineLoginUrl);
+        }
+
+        public ActionResult CALLBACKLOGIN(string code, string state)                                                               //line callback
+        {
+            if (state == "aaa")
+            {
+                #region Api變數宣告
+                WebClient wc = new WebClient();
+                wc.Encoding = Encoding.UTF8;
+                wc.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+                string result = string.Empty;
+                NameValueCollection nvc = new NameValueCollection();
+                #endregion
+                try
+                {
+                    //取回Token
+                    string ApiUrl_Token = "https://api.line.me/oauth2/v2.1/token";
+                    nvc.Add("grant_type", "authorization_code");
+                    nvc.Add("code", code);
+                    nvc.Add("redirect_uri", "https://localhost:7266/CustomerMember/CALLBACKLOGIN");
+                    nvc.Add("client_id", "1657900734");
+                    nvc.Add("client_secret", "8a686ccc1f01658c94ff67511e5d46b3");
+                    string JsonStr = Encoding.UTF8.GetString(wc.UploadValues(ApiUrl_Token, "POST", nvc));
+                    LineLoginToken ToKenObj = JsonConvert.DeserializeObject<LineLoginToken>(JsonStr);
+                    wc.Headers.Clear();
+
+                    //取回User Profile
+                    string ApiUrl_Profile = "https://api.line.me/v2/profile";
+                    wc.Headers.Add("Authorization", "Bearer " + ToKenObj.access_token);
+                    string UserProfile = wc.DownloadString(ApiUrl_Profile);
+                    LineProfile ProfileObj = JsonConvert.DeserializeObject<LineProfile>(UserProfile);
+                    NormalMember member = _context.NormalMembers.FirstOrDefault(c => c.LineUserid == ProfileObj.userId);
+                    if (member != null)
+                    {
+                        CLoginViewModel loginmember =new CLoginViewModel();
+                        loginmember.txtAccount = member.Phone;
+                        loginmember.txtPassword = member.Password;
+                        Login(loginmember);
+
+                    }
+                    else
+                    {
+                        CNormalMemberViewModel x = new CNormalMemberViewModel();
+                        NormalMember newmember = new NormalMember();
+                        newmember.LineUserid=ProfileObj.userId;
+                        newmember.MemberName = ProfileObj.displayName;
+                        newmember.MemberPhotoFile = ProfileObj.pictureUrl;
+
+                        return View("Register", newmember);
+
+                    }
+                   
+                }
+                catch (Exception ex)
+                {
+                    string msg = ex.Message;
+                    throw;
+                }
+            }
+            return Redirect("~/Home/CIndex");
+        }
+
+             public class LineLoginToken
+             {
+            public string access_token { get; set; }
+            public int expires_in { get; set; }
+            public string id_token { get; set; }
+            public string refresh_token { get; set; }
+            public string scope { get; set; }
+            public string token_type { get; set; }
+            }
+
+            public class LineProfile
+            {
+            public string userId { get; set; }
+            public string displayName { get; set; }
+            public string pictureUrl { get; set; }
+            public string statusMessage { get; set; }
+         }
+
 
 
 
@@ -197,7 +295,7 @@ namespace prjMSIT145_Final.Controllers
             return View(member);
         }
         [HttpPost]
-        public IActionResult Register(CNormalMemberViewModel vm, IFormFile photo)
+        public IActionResult Register(CNormalMemberViewModel vm, IFormFile photo)                                               //註冊動作
         {
             var data = _context.NormalMembers.Select(c => c.Phone);
             foreach (var i in data)
