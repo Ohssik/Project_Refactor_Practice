4cf9a330cb29f27197baffe1ded130c23582f834
MaiePu <vickexe@gmail.com>
2023-02-17 19:48:00 +0800

    修改

--- prjMSIT145-Final/Controllers/OrderDetialController.cs
+++ prjMSIT145-Final/Controllers/OrderDetialController.cs
@@ -488,7 +488,7 @@ namespace prjMSIT145_Final.Controllers
 
 			ViewData["MerchantOrderNo"] = DateTime.Now.ToString("yyyyMMddHHmmss");  //訂單編號
 			ViewData["ExpireDate"] = DateTime.Now.AddDays(3).ToString("yyyyMMdd"); //繳費有效期限       
-
+            ViewBag.data = "行不行書瑋";
             return View(vm);
         }
     }
